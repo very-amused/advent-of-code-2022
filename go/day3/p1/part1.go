dc4e374e7cfd5db84c232c1fd03164b050b0c549
very-amused <very-amused@pm.me>
2022-12-03 00:14:51 -0500

    Day 1 part 1

--- go/day3/p1/part1.go
+++ go/day3/p1/part1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -18,7 +19,34 @@ func must[T any](v T, e error) T {
 }
 
 // #region Structs
-type Input any
+type Input []Rucksack
+type Rucksack string
+
+func (s Rucksack) commonItem() rune {
+	// Split compartment in 2
+	c1 := s[:len(s)/2]
+	c2 := s[len(s)/2:]
+	// Find common item
+	itemMap := make(map[rune]bool)
+	for _, char := range c1 {
+		itemMap[char] = true
+	}
+	for _, item := range c2 {
+		if itemMap[item] {
+			return item
+		}
+	}
+	panic("Failed to find common item: " + s)
+}
+
+func (s Rucksack) itemPriority(item rune) int {
+	if item >= 'a' && item <= 'z' {
+		return int(item-'a') + 1 // 1-26
+	} else if item >= 'A' && item <= 'Z' {
+		return int(item-'A') + 27 // 27-52
+	}
+	panic("Invalid item char: " + string(item))
+}
 
 // #endregion
 
@@ -27,20 +55,23 @@ func parse() (input Input) {
 	// Open scanner to read input line by line
 	scanner := bufio.NewScanner(must(os.Open(inputFile)))
 
-	// Parse lines
-
 	for scanner.Scan() {
 		l := scanner.Text()
 		if len(l) == 0 {
-
+			continue
 		}
+		input = append(input, Rucksack(l))
 	}
 	return input
 }
 
 // Solve
 func solve(input Input) (solution string) {
-	return solution
+	c := 0
+	for _, sack := range input {
+		c += sack.itemPriority(sack.commonItem())
+	}
+	return strconv.Itoa(c)
 }
 
 func Part1() {
