77f36a4d5565e141be6a8f729062b687eceee8e5
very-amused <very-amused@pm.me>
2022-12-04 12:04:04 -0500

    Day 4 part 1

---
+++ go/day4/p1/part1.go
@@ -0,0 +1,91 @@
+package p1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const inputFile = "input.txt"  // Change to input.txt for final solution
+const ignoreEmptyLines = false // Don't parse blank lines
+
+func must[T any](v T, e error) T {
+	if e != nil {
+		panic(e)
+	}
+	return v
+}
+
+// #region Structs
+type Input []Pair
+type Pair [2]Assignment
+
+func (p Pair) isRedundant() bool {
+	return p[0].contains(p[1]) || p[1].contains(p[0])
+}
+
+type Assignment struct {
+	start int
+	end   int
+}
+
+func (a Assignment) contains(a2 Assignment) bool {
+	return a.start <= a2.start && a.end >= a2.end
+}
+func assignmentFrom(r string) (a Assignment) {
+	parts := strings.Split(r, "-")
+	if len(parts) != 2 {
+		panic("Unexpected number of parts in assignment range: " + r)
+	}
+	a.start = must(strconv.Atoi(parts[0]))
+	a.end = must(strconv.Atoi(parts[1]))
+	return a
+}
+
+// #endregion
+
+// Parse
+func parse() (pairs Input) {
+	// Open scanner to read input line by line
+	scanner := bufio.NewScanner(must(os.Open(inputFile)))
+
+	// Parse lines
+
+	for scanner.Scan() {
+		l := scanner.Text()
+		if len(l) == 0 {
+			continue
+		}
+		parts := strings.Split(l, ",")
+		pairs = append(pairs, Pair{assignmentFrom(parts[0]), assignmentFrom(parts[1])})
+	}
+	return pairs
+}
+
+// Solve
+func solve(pairs Input) (solution string) {
+	c := 0
+	for _, p := range pairs {
+		if p.isRedundant() {
+			c++
+		}
+	}
+	return strconv.Itoa(c)
+}
+
+func Part1() {
+	// Parse
+	pairs := parse()
+
+	// Solve
+	start := time.Now()
+	solution := solve(pairs)
+
+	// Report solve time and solution
+	duration := time.Now().Sub(start)
+	fmt.Printf("Solved in \x1b[34m%s\x1b[0m\n", duration)
+	fmt.Printf("Solution: \x1b[32m%s\x1b[0m\n", solution)
+}
