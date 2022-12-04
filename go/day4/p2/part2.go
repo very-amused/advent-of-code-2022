ed4e7a633603c53ac07bd90204e2825692c97cbd
very-amused <very-amused@pm.me>
2022-12-04 12:08:06 -0500

    Day 4 part 2

--- go/day4/p2/part2.go
+++ go/day4/p2/part2.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -18,12 +20,36 @@ func must[T any](v T, e error) T {
 }
 
 // #region Structs
-type Input any
+type Input []Pair
+type Pair [2]Assignment
+
+func (p Pair) overlaps() bool {
+	return p[0].overlaps(p[1]) || p[1].overlaps(p[0])
+}
+
+type Assignment struct {
+	start int
+	end   int
+}
+
+func (a Assignment) overlaps(a2 Assignment) bool {
+	return (a.start >= a2.start && a.start <= a2.end) ||
+		(a.end <= a2.end && a.end >= a2.start)
+}
+func assignmentFrom(r string) (a Assignment) {
+	parts := strings.Split(r, "-")
+	if len(parts) != 2 {
+		panic("Unexpected number of parts in assignment range: " + r)
+	}
+	a.start = must(strconv.Atoi(parts[0]))
+	a.end = must(strconv.Atoi(parts[1]))
+	return a
+}
 
 // #endregion
 
 // Parse
-func parse() (input Input) {
+func parse() (pairs Input) {
 	// Open scanner to read input line by line
 	scanner := bufio.NewScanner(must(os.Open(inputFile)))
 
@@ -32,24 +58,32 @@ func parse() (input Input) {
 	for scanner.Scan() {
 		l := scanner.Text()
 		if len(l) == 0 {
-
+			continue
 		}
+		parts := strings.Split(l, ",")
+		pairs = append(pairs, Pair{assignmentFrom(parts[0]), assignmentFrom(parts[1])})
 	}
-	return input
+	return pairs
 }
 
 // Solve
-func solve(input Input) (solution string) {
-	return solution
+func solve(pairs Input) (solution string) {
+	c := 0
+	for _, p := range pairs {
+		if p.overlaps() {
+			c++
+		}
+	}
+	return strconv.Itoa(c)
 }
 
 func Part2() {
 	// Parse
-	input := parse()
+	pairs := parse()
 
 	// Solve
 	start := time.Now()
-	solution := solve(input)
+	solution := solve(pairs)
 
 	// Report solve time and solution
 	duration := time.Now().Sub(start)
