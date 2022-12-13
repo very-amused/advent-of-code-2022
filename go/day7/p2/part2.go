a4bdaeeba578d95e6cfac5e501f450dbc7d2e8b0
very-amused <very-amused@pm.me>
2022-12-13 06:59:55 -0500

    Day 7 part 2

--- go/day7/p2/part2.go
+++ go/day7/p2/part2.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -18,29 +19,113 @@ func must[T any](v T, e error) T {
 }
 
 // #region Structs
-type Input any
+type Dir struct {
+	parent   *Dir
+	children map[string]*Dir
+	files    []uint64
+}
+
+func (d *Dir) size() (s uint64) {
+	for _, file := range d.files {
+		s += file
+	}
+	for _, dir := range d.children {
+		s += dir.size()
+	}
+	return s
+}
+
+func (d *Dir) flatten() (children []*Dir) {
+	for _, c := range d.children {
+		children = append(children, c)
+		children = append(children, c.flatten()...)
+	}
+	return children
+}
+
+func (d *Dir) cd(path string) (n *Dir) {
+	if path == ".." {
+		if d.parent != nil {
+			return d.parent
+		}
+		return d // cd .. from root doesn't move
+	}
+	// If the directory has not previously been explored, create it
+	if d.children[path] == nil {
+		d.mkdir(path)
+	}
+	return d.children[path]
+}
+
+func (d *Dir) mkdir(name string) {
+	if d.children == nil {
+		d.children = make(map[string]*Dir)
+	}
+	d.children[name] = &Dir{
+		parent:   d,
+		children: make(map[string]*Dir)}
+}
 
 // #endregion
 
 // Parse
-func parse() (input Input) {
+func parse() (root *Dir) {
 	// Open scanner to read input line by line
 	scanner := bufio.NewScanner(must(os.Open(inputFile)))
 
 	// Parse lines
-
+	scanner.Scan()
+	if l := scanner.Text(); l != "$ cd /" {
+		panic("Input doesn't start with cd to root dir: " + l)
+	}
+	root = &Dir{children: make(map[string]*Dir)}
+	wd := root
 	for scanner.Scan() {
 		l := scanner.Text()
 		if len(l) == 0 {
-
+			continue
+		}
+		{
+			var cdPath string
+			if n, _ := fmt.Sscanf(l, "$ cd %s", &cdPath); n == 1 {
+				wd = wd.cd(cdPath)
+				continue
+			}
+		}
+		{
+			var filename string
+			var size uint64
+			if n, _ := fmt.Sscanf(l, "%d %s", &size, &filename); n == 2 {
+				wd.files = append(wd.files, size)
+				continue
+			}
+		}
+		{
+			var dirname string
+			if n, _ := fmt.Sscanf(l, "dir %s", &dirname); n == 1 {
+				wd.mkdir(dirname)
+				continue
+			}
 		}
 	}
-	return input
+	return root
 }
 
 // Solve
-func solve(input Input) (solution string) {
-	return solution
+func solve(root *Dir) (solution string) {
+	allDirs := root.flatten()
+
+	const diskSpace = 70000000
+	const neededSpace = 30000000
+	usedSpace := root.size()
+	min := usedSpace
+	for _, dir := range allDirs {
+		size := dir.size()
+		if diskSpace-(usedSpace-size) >= neededSpace && size < min {
+			min = size
+		}
+	}
+	return strconv.FormatUint(min, 10)
 }
 
 func Part2() {
