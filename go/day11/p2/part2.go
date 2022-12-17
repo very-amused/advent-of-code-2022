f9dcb8780efde353bf016a446bc6825c42cf8fa3
very-amused <very-amused@pm.me>
2022-12-16 23:41:25 -0500

    Day 11 part 2

--- go/day11/p2/part2.go
+++ go/day11/p2/part2.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -18,29 +21,153 @@ func must[T any](v T, e error) T {
 }
 
 // #region Structs
-type Input any
+type Input []*Monkey
+
+func (i Input) getMod() (M int) {
+	M = 1
+	for _, m := range i {
+		M *= m.mod
+	}
+	return M
+}
+
+type Monkey struct {
+	items     []int
+	operation func(item *int)
+	mod       int
+
+	// T/F throw destination indexes
+	dest1 int
+	dest2 int
+
+	inspectCount int
+}
+
+func (m *Monkey) inspect(index int) {
+	// Monkey inspects item
+	m.operation(&m.items[index])
+	// Increment inspect count
+	m.inspectCount++
+}
+
+// Decide where to throw an item based on worry level
+func (m *Monkey) findDest(index int) (dest int) {
+	if m.items[index]%m.mod == 0 {
+		return m.dest1
+	} else {
+		return m.dest2
+	}
+}
 
 // #endregion
 
 // Parse
+func parseMonkey(s *bufio.Scanner) (monkey *Monkey) {
+	monkey = new(Monkey)
+	var body string
+	getBody := func(s *bufio.Scanner) string {
+		s.Scan()
+		l := s.Text()
+		return strings.Split(l, ": ")[1]
+	}
+	// Parse starting items
+	{
+		body = getBody(s)
+		startingItems := strings.Split(body, ", ")
+		for _, item := range startingItems {
+			monkey.items = append(monkey.items, must(strconv.Atoi(item)))
+		}
+	}
+	// Operation
+	{
+		body = getBody(s)
+		op := strings.Split(strings.Split(body, "= ")[1], " ")[1:]
+		var opValue int
+		if op[1] != "old" {
+			opValue = must(strconv.Atoi(op[1]))
+		}
+		switch op[0][0] {
+		case '*':
+			if op[1] == "old" {
+				monkey.operation = func(item *int) {
+					*item *= *item
+				}
+			} else {
+				monkey.operation = func(item *int) {
+					*item *= opValue
+				}
+			}
+		case '+':
+			if op[1] == "old" {
+				monkey.operation = func(item *int) {
+					*item += *item
+				}
+			} else {
+				monkey.operation = func(item *int) {
+					*item += opValue
+				}
+			}
+		default:
+			panic("Unknown operator: " + op[0])
+		}
+	}
+	// Test
+	{
+		body = getBody(s)
+		must(fmt.Sscanf(body, "divisible by %d", &monkey.mod))
+	}
+	// Possible destinations
+	{
+		getDest := func() int {
+			body = getBody(s)
+			parts := strings.Split(body, " ")
+			return must(strconv.Atoi(parts[len(parts)-1]))
+		}
+		monkey.dest1 = getDest()
+		monkey.dest2 = getDest()
+	}
+	return monkey
+}
+
 func parse() (input Input) {
 	// Open scanner to read input line by line
 	scanner := bufio.NewScanner(must(os.Open(inputFile)))
 
 	// Parse lines
-
 	for scanner.Scan() {
 		l := scanner.Text()
-		if len(l) == 0 {
-
+		if !strings.HasPrefix(l, "Monkey") {
+			continue
 		}
+		monkey := parseMonkey(scanner)
+		input = append(input, monkey)
 	}
 	return input
 }
 
 // Solve
 func solve(input Input) (solution string) {
-	return solution
+	const rounds = 10000
+	for r := 0; r < rounds; r++ {
+		fmt.Println("Round", r)
+		for _, monkey := range input {
+			for range monkey.items {
+				monkey.inspect(0)
+				monkey.items[0] %= input.getMod()
+				dest := monkey.findDest(0)
+				// Throw element to end of items list
+				input[dest].items = append(input[dest].items, monkey.items[0])
+				monkey.items = append(monkey.items[:0], monkey.items[1:]...) // Remove from throwing monkey
+			}
+		}
+	}
+	// Sort monkeys by inspect count
+	sort.Slice(input, func(i, j int) bool {
+		return input[i].inspectCount < input[j].inspectCount
+	})
+	// Multiply 2 highest counts
+	monkeyBusiness := input[len(input)-2].inspectCount * input[len(input)-1].inspectCount
+	return strconv.Itoa(monkeyBusiness)
 }
 
 func Part2() {
